867f946ea32d9c0e0df510c86ca7e86ae10c61f9
youssefbenyaacoub <youssef.benyaacoub@esen.tn>
2025-12-14 20:11:00 +0100

    Initial commit

---
+++ src/components/Project.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+import ProjectDetails from "./ProjectDetails";
+
+const Project = ({
+  title,
+  description,
+  subDescription,
+  href,
+  image,
+  tags,
+  setPreview,
+}) => {
+  const [isHidden, setIsHidden] = useState(false);
+  return (
+    <>
+      <div
+        className="flex-wrap items-center justify-between py-10 space-y-14 sm:flex sm:space-y-0"
+        onMouseEnter={() => setPreview(image)}
+        onMouseLeave={() => setPreview(null)}
+      >
+        <div>
+          <p className="text-2xl">{title}</p>
+          <div className="flex gap-5 mt-2 text-sand">
+            {tags.map((tag) => (
+              <span key={tag.id}>{tag.name}</span>
+            ))}
+          </div>
+        </div>
+        <button
+          onClick={() => setIsHidden(true)}
+          className="flex items-center gap-1 cursor-pointer hover-animation"
+        >
+          Read More
+          <img src="assets/arrow-right.svg" className="w-5" />
+        </button>
+      </div>
+      <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full" />
+      {isHidden && (
+        <ProjectDetails
+          title={title}
+          description={description}
+          subDescription={subDescription}
+          image={image}
+          tags={tags}
+          href={href}
+          closeModal={() => setIsHidden(false)}
+        />
+      )}
+    </>
+  );
+};
+
+Project.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  subDescription: PropTypes.arrayOf(PropTypes.string),
+  href: PropTypes.string,
+  image: PropTypes.string,
+  tags: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+    })
+  ),
+  setPreview: PropTypes.func,
+};
+
+export default Project;
